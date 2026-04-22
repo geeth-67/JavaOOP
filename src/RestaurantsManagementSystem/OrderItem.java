63eacc9286c09d345e77e7a62d10c226792b5960
vishwageethanjana <geeth.me67@gmail.com>
2026-04-22 16:00:46 +0530

    java oop concept 22/04/2026

--- src/RestaurantsManagementSystem/OrderItem.java
+++ src/RestaurantsManagementSystem/OrderItem.java
@@ -2,5 +2,30 @@ package RestaurantsManagementSystem;
 
 public class OrderItem {
 
+    private Product product ; // order item has a product
+    private int quantity;
 
+    public OrderItem ( Product product , int quantity) {
+
+        this.product = product;
+        this.quantity = quantity;
+    }
+
+    public Product getProduct() {
+        return this.product;
+    }
+
+    public double getSubTotal () {
+
+        return this.product.getPrice() * this.quantity ;
+    }
+
+    public void addQuantity (int value) {
+
+        this.quantity += value ;
+    }
+
+    public int getQuantity () {
+        return this.quantity;
+    }
 }
