22fc90c1148c9ffdcff1bf599fe146aa026a80a1
vishwageethanjana <geeth.me67@gmail.com>
2026-04-22 14:24:43 +0530

    java oop concept 22/04/2026

---
+++ src/RestaurantsManagementSystem/Product.java
@@ -0,0 +1,11 @@
+package RestaurantsManagementSystem;
+
+public class Product {
+
+    private String prodID;
+    private String name;
+    private Double price;
+    private String category;
+    private String description;
+    private boolean isAvailable;
+}
