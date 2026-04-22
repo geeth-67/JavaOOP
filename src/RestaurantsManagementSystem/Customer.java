f9930068f08e64fed90f1f9b264fd71d1bd9a303
vishwageethanjana <geeth.me67@gmail.com>
2026-04-22 14:15:55 +0530

    java oop

---
+++ src/RestaurantsManagementSystem/Customer.java
@@ -0,0 +1,17 @@
+package RestaurantsManagementSystem;
+
+public class Customer {
+
+    int custID ;
+    String name ;
+    int age ;
+
+    public Customer (int custID , String name , int age) {
+
+        this.custID = custID ;
+        this.name = name ;
+        this.age = age ;
+    }
+
+
+}
