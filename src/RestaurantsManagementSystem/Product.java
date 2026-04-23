9561f022c556a31cd8e7a1acdd62c1322b7ab79d
vishwageethanjana <geeth.me67@gmail.com>
2026-04-23 09:14:44 +0530

    java oop concept 22/04/2026

--- src/RestaurantsManagementSystem/Product.java
+++ src/RestaurantsManagementSystem/Product.java
@@ -8,4 +8,38 @@ public class Product {
     private String category;
     private String description;
     private boolean isAvailable;
+
+    public Product ( String name , Double price , String category , String description ) {
+
+        this.prodID = "PID" + name;
+        this.name = name;
+        this.price = price;
+        this.category = category;
+        this.description = description;
+        this.isAvailable = true;
+    }
+
+    public String getProdID () {
+        return prodID;
+    }
+
+    public String getName () {
+        return name;
+    }
+
+    public String getCategory () {
+        return category;
+    }
+
+    public String getDescription () {
+        return description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
 }
